b8d520b24e93a2e247ed5ba8bc18e010b6aa6479
talhayavcin <talhayavcin35@gmail.com>
2022-10-31 10:03:11 +0300

    getStarted button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
 
 const Hero = () => (
@@ -12,6 +13,16 @@ const Hero = () => (
             <span className='text-white'>20%</span> Discount For {" "}
             <span className='text-white'>1 Month</span> Account
           </p>
+        </div>
+        <div className='flex flex-row justify-between items-center w-full'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+            The Next <br className='sm:block hidden' /> {" "}
+            <span className='text-gradient'>Generation</span> {" "}
+            Payment Method.
+          </h1>
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+          </div>
         </div>
       </div>
     </section>
